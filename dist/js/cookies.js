5c01aed96f332bc0f8b17f387f0ba6a6376d6002
Praveen Kumar Nayakudi <praveen.n@c2stechs.com>
2021-02-02 10:12:34 +0530

    changes in demo file

--- dist/js/cookies.js
+++ dist/js/cookies.js
@@ -1,8 +1,5 @@
 //  <!-- cookie based authentication starts here -->
-    console.log(getCookie('username'))
-    console.log(getCookie('password'))
-    alert(getCookie('username'))
-    alert(getCookie('password'))
+
     function setCookie(cname,cvalue,exdays) {
       var d = new Date();
       d.setTime(d.getTime() + (exdays*24*60*60*1000));
